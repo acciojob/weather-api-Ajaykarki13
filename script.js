a68655be4c44ea8dc905defd3874cc8eae11ef1a
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-06-16 12:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+
+"lat":51.4875167,
+      "lon":-0.1687007,
+
+	let btn = document.getElementById('btn')
+let display = document.getElementById('weatherData')
+
+btn.addEventListener('click',handleFetch)
+
+function handleFetch(){
+fetch(`https://api.openweathermap.org/data/2.5/weather?
+lat={51.4875167}&lon={-0.1687007}&appid={249329AG24aqUAk3LyNQSKfu1opX4FRjxYPX9
+}`).then(res=>res.json)
+.then(data=> displayData(data.weather))
+}
+
+function displayData(arr)
+	{
+		
+		arr.map((t)=>{
+			display.innerHTML = `Current weather in London: ${t.main} `
+		})
+	}
